d16b792e7541a98683a2785d5288b463a1dcb814
jaiv <jaiv@itu.dk>
2025-12-05 10:42:23 +0100

    run test results

--- main.py
+++ main.py
@@ -273,7 +273,7 @@ def parse_args():
     # Run parameters
     parser.add_argument('--testing_path', type=str, default='results/trained_models/navigating_uncertainty', help="Path for testing results.")
     parser.add_argument('--folder', type=str, default='sac-pen', help="Folder name for the run.")
-    parser.add_argument('--phase', type=str, default='train', help="WandB project name.")
+    parser.add_argument('--phase', type=str, default='test', help="WandB project name.")
     parser.add_argument('--feasibility_recovery', type=lambda x: x == 'True', default=False, help="Enable feasibility recovery.")
     return parser.parse_args()
 
