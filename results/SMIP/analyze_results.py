4f5179a5c92cc0fd08e586fb99eceb4f4117fec8
jaiv <jaiv@itu.dk>
2025-12-05 09:49:43 +0100

    update

--- results/SMIP/analyze_results.py
+++ results/SMIP/analyze_results.py
@@ -9,11 +9,12 @@ parser = argparse.ArgumentParser(description="Scenario Tree Evaluation Parameter
 parser.add_argument('--s', type=int, default=28, help='scenarios')
 parser.add_argument('--p', type=int, default=3, help='Planning horizon or number of periods (p)')
 parser.add_argument('--episodes', type=int, default=30, help='Number of episodes to evaluate')
-parser.add_argument('--perfect_information', type=bool, default=False)
-parser.add_argument('--gen', type=bool, default=True)
+parser.add_argument('--perfect_information', type=bool, default=True)
+parser.add_argument('--gen', type=bool, default=False)
 parser.add_argument('--cv', type=float, default=0.5, help='Coefficient of variation (cv)')
 parser.add_argument('--teu', type=int, default=1000, help='TEU value')
 parser.add_argument('--block_mpp', type=bool, default=False,)
+parser.add_argument('--stochastic_algorithm', type=str, default="multi_stage", help='Stochastic algorithm type')
 args = parser.parse_args()
 
 # Access arguments like variables
@@ -25,14 +26,15 @@ gen = args.gen
 teu = args.teu
 cv = args.cv
 block_mpp = args.block_mpp
+stochastic_algorithm = args.stochastic_algorithm
 
 # path = f'{folder}/cv={cv}'
 base_dir = os.path.dirname(os.path.abspath(__file__))
 folder_test_gen = 'testing' if not gen else 'generalization'
-folder_pi_na = "pi" if perfect_information else "na"
+folder_pi_na = "multi_stage_pi" if perfect_information else "multi_stage_na"
 
 if teu == 1000 and not block_mpp:
-    input_path = os.path.join(base_dir, "navigating_uncertainty", folder_test_gen, )
+    input_path = os.path.join(base_dir, "navigating_uncertainty", "teu1k", folder_pi_na, "instances")
     output_path = os.path.join(base_dir, "navigating_uncertainty", "teu1k", folder_pi_na)
 elif teu == 20000 and block_mpp:
     input_path = os.path.join(base_dir, "scenario_tree", "block_mpp",)
@@ -51,7 +53,7 @@ total_cost = []
 # Load data
 for e in range(episodes):
     if teu == 1000 and not block_mpp:
-        json_path = f'{input_path}/results_scenario_tree_e{e}_s{s}_pi{perfect_information}_gen{gen}.json'
+        json_path = f'{input_path}/results_scenario_tree_teu{teu}_p{p}_e{e}_s{s}_alg{stochastic_algorithm}_pi{perfect_information}_gen{gen}.json'
     elif teu == 20000 and block_mpp:
         json_path = f'{input_path}/results_scenario_tree_teu{teu}_p{p}_e{e}_s{s}_pi{perfect_information}_gen{gen}.json'
     else:
