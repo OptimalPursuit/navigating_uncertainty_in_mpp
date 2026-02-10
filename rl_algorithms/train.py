806640b5aae9de7c7ec24b33328c0c0b697fe8e8
jaiv <jaiv@itu.dk>
2026-02-10 13:31:55 +0100

    add critics for frank-wolfe

--- rl_algorithms/train.py
+++ rl_algorithms/train.py
@@ -230,7 +230,7 @@ def run_training(policy: nn.Module, critic: nn.Module, device:str="cuda", **kwar
             max_alpha=1.0, #[1.0, 10]
             lagrangian_multiplier=lagrangian_multiplier,
             env_init=vars(train_env),   # dict of instance attributes
-            frank_wolfe_improvement=kwargs["projection_type"] == "frank_wolfe",
+            frank_wolfe_improvement=kwargs["training"]["projection_type"] == "frank_wolfe",
         )
     elif kwargs["algorithm"]["type"] == "ppo":
         loss_module = FeasibilityClipPPOLoss(
@@ -244,7 +244,7 @@ def run_training(policy: nn.Module, critic: nn.Module, device:str="cuda", **kwar
             normalize_advantage=True,
             lagrangian_multiplier=lagrangian_multiplier,
             env_init=vars(train_env),   # dict of instance attributes
-            frank_wolfe_improvement=kwargs["projection_type"] == "frank_wolfe",
+            frank_wolfe_improvement=kwargs["training"]["projection_type"] == "frank_wolfe",
         )
     elif kwargs["algorithm"]["type"] == "ddpg":
         # Create the DDPG loss module
