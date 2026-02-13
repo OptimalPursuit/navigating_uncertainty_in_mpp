add jacobian correct flag

main.py
@@ -202,6 +202,7 @@ def initialize_policy_and_critic(config: DotMap, env:nn.Module, device:Union[str
         return_log_prob=True,
         projection_layer=projection_layer,
         projection_type=config.training.projection_type,
+        jacobian_correction=config.training.projection_kwargs.get("jacobian_correction", True),
         spec=env.action_spec,
         revenues=env.revenues,
     )
@@ -298,7 +299,7 @@ def parse_args():
     parser.add_argument('--projection_type', type=str, default="frank_wolfe", help="Projection type.")
     parser.add_argument('--projection_kwargs', type=json.loads, default={
             'alpha': 0.01, 'delta': 0.01, 'max_iter': 300, 'slack_penalty': 10000, 'n_action': 20, 'n_constraints': 25,
-            'use_spectral_eta': False, 'power_iters': 3, 'enable_alpha_map':False, 'enforce_nonneg':False},
+            'use_spectral_eta': False, 'power_iters': 3, 'enable_alpha_map':False, 'enforce_nonneg':False, 'jacobian_correction':True},
                         help="Projection parameters as JSON string.")
 
     # Run parameters

rl_algorithms/projection_prob_actor.py
@@ -30,12 +30,16 @@ class ProjectionProbabilisticActor(ProbabilisticActor):
                  projection_layer: Optional[nn.Module] = None,
                  projection_type: Optional[str] = None,
                  revenues: Optional[Tensor] = None,
+                 jacobian_correction: bool = True,
                  **kwargs):
         super().__init__(module, in_keys, out_keys, spec=spec, **kwargs)
 
         # Initialize projection layer
         self.projection_layer = projection_layer
         self.projection_type = projection_type.lower()
+        self.jacobian_correction = jacobian_correction
+        print("jacobian_correction:", self.jacobian_correction)
+        breakpoint()
 
         # Initialize clipped Gaussian
         initial_loc = torch.zeros(spec.shape, device=spec.device, dtype=spec.dtype)
@@ -328,10 +332,11 @@ class ProjectionProbabilisticActor(ProbabilisticActor):
             sample_logp = logp_full.sum(dim=-1)  # shape [...]
 
             # Apply jacobian correction in full space if implemented for projection_type
-            J = self.handle_jacobian_adjustment(out)  # returns [..., N, N] or None
-            if J is not None:
-                sample_logp = logp_full.sum(dim=-1)
-                sample_logp = self.jacobian_adaptation(sample_logp, jacobian=J).clamp(min=-50.0)
+            if self.jacobian_correction:
+                J = self.handle_jacobian_adjustment(out)  # returns [..., N, N] or None
+                if J is not None:
+                    sample_logp = logp_full.sum(dim=-1)
+                    sample_logp = self.jacobian_adaptation(sample_logp, jacobian=J).clamp(min=-50.0)
 
             # out["log_prob"] = logp_full
             out["sample_log_prob"] = sample_logp
@@ -391,8 +396,7 @@ class ProjectionProbabilisticActor(ProbabilisticActor):
         """
 
         # ---- no jacobian: fully vectorized ----
-        use_jac = (handle_jacobian_adjustment is not None) and (jacobian_adaptation is not None)
-        if not use_jac:
+        if not self.jacobian_correction:
             return out["log_prob"], out["sample_log_prob"]
 
         # Shapes